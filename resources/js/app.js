acab45f419c716b8e657cae7c5bada2af4e73a57
Supardi <supardi9512@gmail.com>
2022-06-19 17:54:34 +0700

    create vue routing and read (get) data

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,36 @@
-import './bootstrap';
+require('./bootstrap');
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import { createRouter, createWebHistory } from "vue-router";
+
+import IndexComponent from './components/posts/Index.vue';
+import CreateComponent from './components/posts/Create.vue';
+import EditComponent from './components/posts/Edit.vue';
+
+const routes = [
+    {
+        name: 'posts',
+        path: '/',
+        component: IndexComponent
+    },
+    {
+        name: 'create',
+        path: '/create',
+        component: CreateComponent
+    },
+    {
+        name: 'edit',
+        path: '/edit/:id',
+        component: EditComponent
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(VueAxios, axios).use(router).mount('#app')
